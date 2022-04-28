b635c39cd15696cd692ae9f18e9e93511066cf35
BerkayYelkanat <104393221+BerkayYelkanat@users.noreply.github.com>
2022-04-28 23:47:47 +0300

    Add files via upload

---
+++ kaybolandaire.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+		document.getElementById("kirmiziDaire").onclick=function(){
+			document.getElementById("kirmiziDaire").style.display="none";
+		}
+		
+		document.getElementById("sariDaire").onclick=function(){
+			document.getElementById("sariDaire").style.display="none";
+		}
+		
+		
+		
+	</script>
