a00c7169fe249e8c0bf8d22ef5a20dbc976108b9
BerkayYelkanat <104393221+BerkayYelkanat@users.noreply.github.com>
2022-04-28 23:49:35 +0300

    Update kaybolandaire.js

--- kaybolandaire.js
+++ kaybolandaire.js
@@ -1,12 +1,7 @@
-<script type="text/javascript">
-		document.getElementById("kirmiziDaire").onclick=function(){
+document.getElementById("kirmiziDaire").onclick=function(){
 			document.getElementById("kirmiziDaire").style.display="none";
 		}
 		
 		document.getElementById("sariDaire").onclick=function(){
 			document.getElementById("sariDaire").style.display="none";
 		}
-		
-		
-		
-	</script>
